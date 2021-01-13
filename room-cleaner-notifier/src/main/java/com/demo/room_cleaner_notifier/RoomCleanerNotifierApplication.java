c4c4ebbf1ace64e358df4826901a399156d62c64
Santosh Mathew <smathew@captechventures.com>
2021-01-13 14:00:37 -0500

    Add room cleaner notifier module

---
+++ room-cleaner-notifier/src/main/java/com/demo/room_cleaner_notifier/RoomCleanerNotifierApplication.java
@@ -0,0 +1,13 @@
+package com.demo.room_cleaner_notifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomCleanerNotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomCleanerNotifierApplication.class, args);
+	}
+
+}

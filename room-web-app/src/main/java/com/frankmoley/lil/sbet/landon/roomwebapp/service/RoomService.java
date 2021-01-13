a64a7ed6015aa2f1603c1991c007c0839735085b
Santosh Mathew <smathew@captechventures.com>
2021-01-13 12:28:16 -0500

    Create queue consumer

--- room-web-app/src/main/java/com/frankmoley/lil/sbet/landon/roomwebapp/service/RoomService.java
+++ room-web-app/src/main/java/com/frankmoley/lil/sbet/landon/roomwebapp/service/RoomService.java
@@ -18,4 +18,8 @@ public class RoomService {
     public List<Room> getAllRooms(){
         return roomRepository.findAll();
     }
+
+    public Room getById(long id) {
+        return roomRepository.findById(id).get();
+    }
 }

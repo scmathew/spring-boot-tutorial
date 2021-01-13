1416a1762ca08dfbec17c10b6ab5434f64cda793
Santosh Mathew <smathew@captechventures.com>
2021-01-13 11:45:52 -0500

    Add rabbit mq bin scripts

--- bin/start_postgres.sh
+++ bin/start_postgres.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-which psql > /dev/null || (echo "Please ensure that postgres client is in your PATH" && exit 1)
+which docker > /dev/null || (echoerr "Please ensure that docker is in your PATH" && exit 1)
 
 mkdir -p $HOME/docker/volumes/postgres
 rm -rf $HOME/docker/volumes/postgres/data
@@ -9,5 +9,8 @@ rm -rf $HOME/docker/volumes/postgres/data
 docker run --rm --name pg-docker -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=dev -d -p 5432:5432 -v $HOME/docker/volumes/postgres:/var/lib/postgresql postgres
 sleep 3
 export PGPASSWORD=postgres
-psql -U postgres -d dev -h localhost -f schema.sql
-psql -U postgres -d dev -h localhost -f data.sql
+
+docker cp ./schema.sql pg-docker:/docker-entrypoint-initdb.d/schema.sql
+docker cp ./data.sql pg-docker:/docker-entrypoint-initdb.d/data.sql
+docker exec -u postgres pg-docker psql dev postgres -f docker-entrypoint-initdb.d/schema.sql
+docker exec -u postgres pg-docker psql dev postgres -f docker-entrypoint-initdb.d/data.sql
